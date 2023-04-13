844beadbb26151cd0c81cc695e40648139340848
olatunji-weber <olatunji.weber@gmail.com>
2023-04-13 10:44:38 +0200

    Added a Link to find the project on my GitHub

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     img = models.ImageField(upload_to='pics')
+    github_link = models.CharField(max_length=1020)
 
     def __str__(self):
         return self.title
